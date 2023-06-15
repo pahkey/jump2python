5dd7b3cfae4278c414e3c0baea13d067f0e6f5b7
EungYong Park <pahkey@gmail.com>
2023-06-15 12:09:37 +0900

    Create iterator.py

---
+++ 07-3/iterator.py
@@ -0,0 +1,20 @@
+# iterator.py
+class MyItertor:
+    def __init__(self, data):
+        self.data = data
+        self.position = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.position >= len(self.data):
+            raise StopIteration
+        result = self.data[self.position]
+        self.position += 1
+        return result
+
+if __name__ == "__main__":
+    i = MyItertor([1,2,3])
+    for item in i:
+        print(item)
