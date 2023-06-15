efc5cd27c758cc54693023a95fb40462ce6d6389
EungYong Park <pahkey@gmail.com>
2023-06-15 11:04:13 +0900

    Create marks2.py

---
+++ 03-3/marks2.py
@@ -0,0 +1,9 @@
+# marks2.py
+marks = [90, 25, 67, 45, 80]
+
+number = 0 
+for mark in marks: 
+    number = number +1 
+    if mark < 60:
+        continue 
+    print("%d번 학생 축하합니다. 합격입니다. " % number)
