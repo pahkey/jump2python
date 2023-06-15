b853da63626d4510ea50916e8b8c0c38dafffe92
EungYong Park <pahkey@gmail.com>
2023-06-15 11:48:22 +0900

    Create thread_test.py

---
+++ 05-6/thread_test.py
@@ -0,0 +1,23 @@
+# thread_test.py
+import time
+import threading
+
+def long_task():
+    for i in range(5):
+        time.sleep(1)
+        print("working:%s\n" % i)
+
+print("Start")
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=long_task)
+    threads.append(t)
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()  # join으로 스레드가 종료될때까지 기다린다.
+
+print("End")
