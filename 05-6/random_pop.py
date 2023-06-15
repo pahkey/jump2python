be42f275cca6a58a3e7305f7dc89c526b9a99363
EungYong Park <pahkey@gmail.com>
2023-06-15 11:39:44 +0900

    Create random_pop.py

---
+++ 05-6/random_pop.py
@@ -0,0 +1,10 @@
+# random_pop.py
+import random
+def random_pop(data):
+    number = random.randint(0, len(data)-1)
+    return data.pop(number)
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4, 5]
+    while data: 
+        print(random_pop(data))
