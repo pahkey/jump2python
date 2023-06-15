9b9d037a1e4d10db1b31fdc79ddeb17e0decc2c2
EungYong Park <pahkey@gmail.com>
2023-06-15 10:53:41 +0900

    Create simple.py

---
+++ 01-2/simple.py
@@ -0,0 +1,9 @@
+languages = ['python', 'perl', 'c', 'java']
+
+for lang in languages:
+    if lang in ['python', 'perl']:
+        print("%6s need interpreter" % lang)
+    elif lang in ['c', 'java']:
+        print("%6s need compiler" % lang)
+    else:
+        print("should not reach here")
