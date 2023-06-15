09c97b13c1fc578fdc3f3ff92c1323426a4217f1
EungYong Park <pahkey@gmail.com>
2023-06-15 11:55:31 +0900

    Create paging.py

---
+++ 06-3/paging.py
@@ -0,0 +1,12 @@
+# paging.py
+
+def get_total_page(m, n):
+    if m % n == 0:
+        return m // n
+    else:
+        return m // n + 1
+
+print(get_total_page(5, 10))
+print(get_total_page(15, 10))
+print(get_total_page(25, 10))
+print(get_total_page(30, 10))  # 3 출력
