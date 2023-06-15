98c694d971227a6725bb552550d280986861bd43
EungYong Park <pahkey@gmail.com>
2023-06-15 11:06:08 +0900

    Create default2.py

---
+++ 04-1/default2.py
@@ -0,0 +1,8 @@
+# default2.py
+def say_myself(name, man=True, old): 
+    print("나의 이름은 %s 입니다." % name) 
+    print("나이는 %d살입니다." % old) 
+    if man: 
+        print("남자입니다.") 
+    else: 
+        print("여자입니다.")
