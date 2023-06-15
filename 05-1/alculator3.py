2b22236d920f3a009c295ea693cc695787ce9808
EungYong Park <pahkey@gmail.com>
2023-06-15 11:15:05 +0900

    Create alculator3.py

---
+++ 05-1/alculator3.py
@@ -0,0 +1,16 @@
+# calculator3.py
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+        return self.result
+
+cal1 = Calculator()
+cal2 = Calculator()
+
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal2.add(3))
+print(cal2.add(7))
