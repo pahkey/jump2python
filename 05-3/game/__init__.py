b88a14d2cf20cfa00f4574715f6b716aa4192166
EungYong Park <pahkey@gmail.com>
2023-06-15 11:21:59 +0900

    Update __init__.py

--- 05-3/game/__init__.py
+++ 05-3/game/__init__.py
@@ -1,1 +1,10 @@
+# C:/doit/game/__init__.py
+from .graphic.render import render_test
 
+VERSION = 3.5
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}.")
+
+# 여기에 패키지 초기화 코드를 작성한다.
+print("Initializing game ...")
