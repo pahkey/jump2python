41f978310789e7b65ad57bbf3b91b42e97433f79
EungYong Park <pahkey@gmail.com>
2023-06-15 11:59:26 +0900

    Create sub_dir_search.py

---
+++ 06-6/sub_dir_search.py
@@ -0,0 +1,18 @@
+# C:/doit/sub_dir_search.py
+import os
+
+def search(dirname):
+    try:
+        filenames = os.listdir(dirname)
+        for filename in filenames:
+            full_filename = os.path.join(dirname, filename)
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                ext = os.path.splitext(full_filename)[-1]
+                if ext == '.py': 
+                    print(full_filename)
+    except PermissionError:
+        pass
+
+search("c:/")
