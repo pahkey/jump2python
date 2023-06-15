ad9bebcce3cb05f9b511d1cc4255407f6ddc1222
EungYong Park <pahkey@gmail.com>
2023-06-15 11:17:34 +0900

    Update mod1.py

--- 05-2/mod1.py
+++ 05-2/mod1.py
@@ -1,6 +1,10 @@
 # mod1.py
-def add(a, b):
-    return a + b
+def add(a, b): 
+    return a+b
 
 def sub(a, b): 
     return a-b
+
+if __name__ == "__main__":
+    print(add(1, 4))
+    print(sub(4, 2))
