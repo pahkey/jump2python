f4a31ab2cfef8fe4209a1e80c8120e9746259cf4
EungYong Park <pahkey@gmail.com>
2023-06-15 11:02:44 +0900

    Create coffee.py

---
+++ 03-2/coffee.py
@@ -0,0 +1,16 @@
+# coffee.py
+coffee = 10
+while True:
+    money = int(input("돈을 넣어 주세요: "))
+    if money == 300:
+        print("커피를 줍니다.")
+        coffee = coffee -1
+    elif money > 300:
+        print("거스름돈 %d를 주고 커피를 줍니다." % (money -300))
+        coffee = coffee -1
+    else:
+        print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+        print("남은 커피의 양은 %d개 입니다." % coffee)
+    if coffee == 0:
+        print("커피가 다 떨어졌습니다. 판매를 중지 합니다.")
+        break
