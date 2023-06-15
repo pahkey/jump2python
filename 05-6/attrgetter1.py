59000980f4952cb4f7a15c1de83952647ee28d09
EungYong Park <pahkey@gmail.com>
2023-06-15 11:47:28 +0900

    Create attrgetter1.py

---
+++ 05-6/attrgetter1.py
@@ -0,0 +1,16 @@
+# attrgetter1.py
+from operator import attrgetter
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+students = [
+    Student('jane', 22, 'A'),
+    Student('dave', 32, 'B'),
+    Student('sally', 17, 'B'),
+]
+
+result = sorted(students, key=attrgetter('age'))
