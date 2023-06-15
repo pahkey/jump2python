ec11f97c4d4efe11fcbca76555ca3b9bdd8f3a4a
EungYong Park <pahkey@gmail.com>
2023-06-15 11:29:55 +0900

    Create try_else.py

---
+++ 05-4/try_else.py
@@ -0,0 +1,10 @@
+# try_else.py
+try:
+    age=int(input('나이를 입력하세요: '))
+except:
+    print('입력이 정확하지 않습니다.')
+else:
+    if age <= 18:
+        print('미성년자는 출입금지입니다.')
+    else:
+        print('환영합니다.')
