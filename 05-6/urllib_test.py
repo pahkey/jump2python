b9aa9f9f6f10fe04670ba7e5f54b1ee629c32754
EungYong Park <pahkey@gmail.com>
2023-06-15 11:49:34 +0900

    Create urllib_test.py

---
+++ 05-6/urllib_test.py
@@ -0,0 +1,8 @@
+# urllib_test.py
+import urllib.request
+
+def get_wikidocs(page):
+    resource = 'https://wikidocs.net/{}'.format(page)
+    with urllib.request.urlopen(resource) as s:
+        with open('wikidocs_%s.html' % page, 'wb') as f:
+            f.write(s.read())
