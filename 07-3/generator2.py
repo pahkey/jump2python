1c523b8639e323d5884b01f7ac42ac984f81af09
EungYong Park <pahkey@gmail.com>
2023-06-15 12:10:35 +0900

    Create generator2.py

---
+++ 07-3/generator2.py
@@ -0,0 +1,10 @@
+# generator2.py
+import time
+
+def longtime_job():
+    print("job start")
+    time.sleep(1)
+    return "done"
+
+list_job = (longtime_job() for i in range(5))
+print(next(list_job))
