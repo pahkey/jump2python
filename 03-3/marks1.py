350026f111e7363e65e7f0b54f81c249b3c53a38
EungYong Park <pahkey@gmail.com>
2023-06-15 11:03:35 +0900

    Create marks1.py

---
+++ 03-3/marks1.py
@@ -0,0 +1,10 @@
+# marks1.py
+marks = [90, 25, 67, 45, 80]   # 학생들의 시험 점수 리스트
+
+number = 0   # 학생에게 붙여 줄 번호
+for mark in marks:   # 90, 25, 67, 45, 80을 순서대로 mark에 대입
+    number = number +1 
+    if mark >= 60: 
+        print("%d번 학생은 합격입니다." % number)
+    else: 
+        print("%d번 학생은 불합격입니다." % number)
