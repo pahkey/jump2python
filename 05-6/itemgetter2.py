470b6441fb3d2391b4a57da67fe5f1823f93c505
EungYong Park <pahkey@gmail.com>
2023-06-15 11:47:05 +0900

    Create itemgetter2.py

---
+++ 05-6/itemgetter2.py
@@ -0,0 +1,11 @@
+# itemgetter2.py
+from operator import itemgetter
+
+students = [
+    {"name": "jane", "age": 22, "grade": 'A'},
+    {"name": "dave", "age": 32, "grade": 'B'},
+    {"name": "sally", "age": 17, "grade": 'B'},
+]
+
+result = sorted(students, key=itemgetter('age'))
+print(result)
