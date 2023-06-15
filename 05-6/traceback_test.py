642ed22c040868efdc5de1c094d55e963bd9f965
EungYong Park <pahkey@gmail.com>
2023-06-15 11:48:46 +0900

    Create traceback_test.py

---
+++ 05-6/traceback_test.py
@@ -0,0 +1,17 @@
+# traceback_test.py
+import traceback
+
+def a():
+    return 1/0
+
+def b():
+    a()
+
+def main():
+    try:
+        b()
+    except:
+        print("오류가 발생했습니다.")
+        print(traceback.format_exc())
+
+main()
